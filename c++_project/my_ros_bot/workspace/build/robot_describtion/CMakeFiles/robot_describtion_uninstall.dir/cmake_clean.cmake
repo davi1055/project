af21945f92b350543e1fe1a47896f478ec159f6c
davi1055 <davicheng1114@gmail.com>
2024-05-26 21:33:08 +0800

    davi

--- c++_project/my_ros_bot/workspace/build/robot_describtion/CMakeFiles/robot_describtion_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/robot_describtion_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/robot_describtion_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
